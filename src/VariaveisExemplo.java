3e28b2e4a493aae9e217e091977a9c9c077336b3
Italo Santana Da Silva <alemao5958@gmail.com>
2025-05-26 16:44:45 -0400

    Variaves Basicas.

---
+++ src/VariaveisExemplo.java
@@ -0,0 +1,15 @@
+public class VariaveisExemplo {
+    public static void main(String[] args) {
+        int idade = 20;
+        double altura = 1.75;
+        char inicial = 'J';
+        boolean estudandoJava = true;
+        String nome = "Maria";
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Inicial do nome: " + inicial);
+        System.out.println("Está estudando Java? " + estudandoJava);
+    }
+}
